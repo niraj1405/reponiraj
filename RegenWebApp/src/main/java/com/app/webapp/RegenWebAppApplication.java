e6fdd4f5c88675b76f02e3393e04f5c5981dcd54
niraj1405 <nirajkumarverma.78@gmail.com>
2024-03-29 18:42:08 +0530

    Commited

---
+++ RegenWebApp/src/main/java/com/app/webapp/RegenWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegenWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegenWebAppApplication.class, args);
+	}
+
+}
---
+++ RegenWebApp/src/main/java/com/app/webapp/controller/WebController.java
@@ -0,0 +1,17 @@
+package com.app.webapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/webapp")
+public class WebController {
+	
+	
+    @GetMapping("/greet")
+	public String greetings()
+	{
+		return "Hello Docker";
+	}
+}
